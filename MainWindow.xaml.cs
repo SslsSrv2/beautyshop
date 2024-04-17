fbd723e44ed71daacfac0f17dd3659819194bfbb
SslsSrv2 <96552511+SslsSrv2@users.noreply.github.com>
2024-04-17 19:10:42 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp4.AppDataFile;
+using WpfApp4.AppDataFile.Pages;
+using WpfApp4.Pages;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ConnectObj.conObj = new BeautyShop2Entities();
+            FrameObf.fraimeMain = FrameMain;
+            FrameMain.Navigate(new PageLogin());
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                FrameMain.GoBack();
+            }
+            catch (Exception)
+            {
+               MessageBox.Show("Невозможно вернуться");
+            }
+        }
+        private void btnFaq_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void btnProduct_Click(object sender, RoutedEventArgs e)
+        {
+            FrameMain.Navigate(new PageProduct());
+        }
+    }
+}
